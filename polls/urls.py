7ba29498553087edf5b7a2c33824f2413568b929
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2023-10-31 18:43:20 +0100

    czwarte

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/persons/', views.PersonViewSet.as_view({'get': 'list'}), name='person-list'),
+    path('api/teams/', views.TeamViewSet.as_view({'get': 'list'}), name='team-list'),
 ]
