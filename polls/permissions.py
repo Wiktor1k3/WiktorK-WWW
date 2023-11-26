5c1b7f82fc20edea6366fcdf4fc4adccd5dc5dfa
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2023-11-26 19:15:51 +0100

    siódme

--- polls/permissions.py
+++ polls/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+import copy
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -7,3 +8,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # if request.method in permissions.SAFE_METHODS:
         #     return True
         return obj.wlasciciel == request.user
+
+class CustomDjangoModelPermissions(permissions.DjangoModelPermissions):
+
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
